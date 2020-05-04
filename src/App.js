16bea4d14326ff339eb22c8cca85daf0de447c76
Hannie <sailor_121108@hotmail.com>
2020-05-03 21:00:19 -0500

    new component details client

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Containers/Home';
 import CustomersContainer from './Containers/CustomersContainer';
+import CustomContent from './Containers/CustomContent';
 import './App.css';
 
 
 class App extends Component{
 
   renderHome = () => <Home />
-  renderCustomerContainer = () => <h2>Customer Customer Container</h2>;
   renderCustomerListContainer = () => <h2>Customer List Container</h2>;
-  renderCustomerNewContainer = () => <h2>Customer List Container</h2>;
 
   render(){
     return (
@@ -18,10 +17,12 @@ class App extends Component{
         <div>
           <Route exact path="/" component={this.renderHome} />
           <Route exact path="/customers" component={CustomersContainer} />
-          {/*<Switch>
-            <Route path="/customers/nuevo" component={this.renderCustomerNewContainer()} />
-            <Route path="/customers/:dni" component={this.renderCustomerListContainer()} />
-          </Switch>*/}
+          <Switch>
+            {/*<Route path="/customers/nuevo" component={this.renderCustomerNewContainer()} />*/}
+            <Route 
+              path="/customers/:dni" 
+              render={props => <CustomContent dni={props.match.params.dni} />} />
+          </Switch>
         </div>
       </Router>
     );
--- src/Containers/CustomContent.js
+++ src/Containers/CustomContent.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import AppFrame from '../Components/AppFrame';
+import { getCustomerByDni } from '../Selectors/Custom'
+import { Route } from 'react-router-dom';
+import CustomerEdit from './../Components/CustomerEdit';
+import CustomerData from './../Components/CustomerData';
 
 class CustomContent extends Component {
+
+    renderBodyClient = () => (
+        <Route 
+            path="/customers/:dni/editar"
+            children={
+                ({ match }) => {
+                    const CustomerControl = match ? CustomerEdit : CustomerData;
+                    return <CustomerControl { ...this.props.customer} /> 
+                }
+            }
+        />
+    )
+    //<p>Datos del client "{this.props.customer.name}" </p>
+
     render() {
         return (
             <div>
-                
+                <AppFrame 
+                    header={`Cliente ${this.props.dni}`}
+                    body={this.renderBodyClient()}    
+                />
             </div>
         );
     }
 }
 
 CustomContent.propTypes = {
-
+    dni: PropTypes.string.isRequired,
+    customer: PropTypes.object.isRequired,
 };
 
-export default CustomContent;
+const mapStateToProps = (state, props) => ({
+    customer: getCustomerByDni(state, props)
+});
+
+export default connect(mapStateToProps, null)(CustomContent);
